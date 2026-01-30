852fa95976be96f9762055acff3c1a71a3adaa53
Mohd Azeem Khan <azeemkhan1597@gmsil.com>
2026-01-30 09:54:59 +0530

    fetch api revision

--- day_01/promises/index.js
+++ day_01/promises/index.js
@@ -1,14 +1,38 @@
-const mypromise = new Promise((resolve, reject) => {
-    let marks =33;
-    if(marks >33){
-        resolve("You are passed");
-    }    else{
-        reject("You are failed");
-    }           
-});
-mypromise.then((message) => {
-    console.log(message);
+// const myPromise=new Promise((resolve,reject)=>{
+//     let marks=34;
+//     if(marks>33){
+//         resolve("pass");
+//     }else{
+//         reject("Fail");
+//     }
+// }
+
+// )
+
+const f1 = async()=>{
+    try{
+        const res=await myPromise;
+        console.log(msg);
+    }catch(error){
+        console.log("Some error occurred: ",error);
+    }
 }
-).catch((error) => {
-    console.log("some error  occured",error);
-});
+// f1();
+
+
+const getData=async()=>{
+   
+   try{
+    const response=await fetch("https://fakestoreapi.com/products");
+     const data=await response.json();
+     return data;
+   }catch(error){
+    console.log("Some error occurred... ");
+   }
+}    
+fetch("https://fakestoreapi.com/products")
+.then((res)=>console.log(res.json())) 
+// const output=getData();
+// console.log(output);    
+// myPromise.then((msg)=>console.log(msg))
+// .catch((error)=>console.log("Some error occurred: ",error));
