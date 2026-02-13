b82f3574f2c6275066f0c9aea2a7d4ca1a31fb50
Mohd Azeem Khan <azeemkhan1597@gmsil.com>
2026-02-13 09:56:26 +0530

    day4

--- day_03/dir.js
+++ day_03/dir.js
@@ -1,22 +1,30 @@
-import fs from 'fs/promises';
-const readmkdir = async (path) => {    try{
-        const data = await fs.readdir(path);
-        console.log(data)
-console.log("dir has been removed successfully");
+import fs from "fs/promises";
+const removeDir = async(path)=>{
+    try {
+       await fs.rmdir(path);
+       console.log("dir has been removed");       
+    } catch (error) {
+        console.log("unable to remove");       
     }
-    catch(err){
-        console.log("Some error occurred while reading directory: ");
-    }   
 }
-removedir("./day_03");    
-const makedir = async (path) => {
-    try{
-        await fs.promises.mkdir(path)=>{
-            try{
-                await fs.mkdir(path);
-                console.log("Directory created successfully");
-            }
-            catch(err){
-                console.log("Some error occurred while creating directory: ");
-            }
-        }
+removeDir("../day_06");
+
+const readDir = async(path)=>{
+    try {
+       const data = await fs.readdir(path);
+       console.log(data);       
+    } catch (error) {
+        console.log("unable to read");       
+    }
+}
+readDir("../day_02");
+
+
+const makeDir = async(path)=>{
+   try {
+    await fs.mkdir(path);
+    console.log("Directory has been created successfully.");    
+   } catch (error) {
+    console.log("unable to make dir");   
+   }
+}
---
+++ day_04/README.md
@@ -0,0 +1,1 @@
+# ABES-FSD-C-6TH
